4070967e38d678a79ad1007bdfd5b1edea8cbd95
ale lisi <10621058@mail.polimi.it>
2021-01-04 00:23:07 +0100

    working full gamification trigger
    Took 25 minutes

--- src/main/java/it/polimi/db2/auxiliary/triggers/ComputeMandatoryPoints.sql
+++ src/main/java/it/polimi/db2/auxiliary/triggers/ComputeMandatoryPoints.sql
@@ -1,17 +1,21 @@
-# MY SQL CODE FOR TRIGGER 1 #
-create trigger "Compute_Mandatory_Points" after insert on answer
-    for each row
-    begin
-        declare _points integer;
+CREATE DEFINER=`root`@`%` TRIGGER `Compute_Mandatory_Points` AFTER INSERT ON `answer` FOR EACH ROW BEGIN
 
-        if (exists(select * from reward as R where R.userId = new.userId and R.productId = new.productId))
-            then set _points = 1 + (select points from reward as R where R.userId = new.userId and R.productId = new.productId);
-            else set _points = 1;
+    declare _points integer;
+    declare _firstTime boolean;
+    declare _load integer;
 
-        end if;
+    set _load = 2 - (select isMandatory from question where questionId = new.questionId and productId = new.productId); #first point ever
 
+    if exists(select * from reward as R where R.userId = new.userId and R.productId = new.productId) then
+        set _points = _load + (select points from reward as R where R.userId = new.userId and R.productId = new.productId);
         update reward
         set points = _points
         where (userId = new.userId and productId = new.productId);
+    else set _points =  _load;
+    insert into reward (userId, productId, points) values  (new.userId, new.productId, _points);
 
-    end;
+    end if;
+
+
+
+END
